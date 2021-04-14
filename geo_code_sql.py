bf630e415b300fa61dc5271f0e96026c51a8965f
Emmanuel Kaunda <ekaunda@tesla.com>
2021-04-14 17:04:22 +0200

    location Bug fix
    fixed bug that was blowing up the program anytime an incorrect city address was input

--- geo_code_sql.py
+++ geo_code_sql.py
@@ -9,7 +9,7 @@ import sqlite3
 conn = sqlite3.connect('HdDB.db')
 c = conn.cursor()
 
-c.execute('''CREATE TABLE IF NOT EXISTS HDDistances(ReferenceNumber number PRIMARY KEY,
+c.execute('''CREATE TABLE IF NOT EXISTS HDDistances(ReferenceNumber number,
 	Vin text, 
 	CustomerName text, 
 	Address text, 
@@ -49,9 +49,12 @@ def create_df(file_read):
 	return encoded_df
 
 def converter(geocode_df):
+
 	print('Geocoding Addresses...')
 	geocode_df['Geocode'] = geocode_df.apply(geo_code, axis=1)
-		
+	#geocode_df = df[['ReferenceNumber','Vin', 'CustomerName', 'Address', 'OrderType', 'VehicleSubStatus','Geocode', 'EncodedAddress', 'DeliveryZip', 'DeliveryCity']]
+
+
 	#Convert Geocoded Column into DistanceMatrix values
 	print('Calculating Distance Matrices...')
 	geocode_df[['TravelTime', 'Distance']] = geocode_df.apply(distance_matrix, axis=1, result_type='expand')
@@ -81,8 +84,16 @@ def geo_code(data):
 
 	print(call.status_code, call.reason)
 	geo_json = json.loads(call.text)
-	coords = [x for x in geo_json['features'][0]['geometry']['coordinates']]
-	return coords # longitude,latitude
+	try:
+		coords = [x for x in geo_json['features'][0]['geometry']['coordinates']]
+		print(coords)
+		return coords # longitude,latitude
+		try:
+			print('There was a problem GEOCODING address, please ensure address is correctly written: ', data['DeliveryCity'])
+			coords = []
+			return coords
+		except Exception as e: print(e)
+	except Exception as e: print(e)
 
 #Calculate distance Matrix
 def distance_matrix(geo_data):
@@ -91,6 +102,7 @@ def distance_matrix(geo_data):
 	#Amflughafen = [13.541659971638602, 52.37798921652795]
 	location = {'Am-Flughafen': [13.541659971638602, 52.37798921652795]} #dictionary of location coordinates
 
+
 	body = {"locations":[location['Am-Flughafen'], geocode_column],"metrics":["distance","duration"],"units":"km"}
 
 	headers = {
@@ -98,16 +110,23 @@ def distance_matrix(geo_data):
 		    'Authorization': '5b3ce3597851110001cf62487e38ccf07c2b4551ba276fa19e10ecdb',
 		    'Content-Type': 'application/json; charset=utf-8'
 		}
-	call = requests.post('https://api.openrouteservice.org/v2/matrix/driving-car', json=body, headers=headers)
 
-	print(call.status_code, call.reason)
-	z = json.loads(call.text)
-	#print(z)
+	try:
+		call = requests.post('https://api.openrouteservice.org/v2/matrix/driving-car', json=body, headers=headers)
 
-	time_to_loc = int(z['durations'][0][1] / 60)
-	distance_to_loc = z['distances'][0][1]
+		print(call.status_code, call.reason)
+		z = json.loads(call.text)
+		#print(z)
 
-	return time_to_loc, distance_to_loc
+		time_to_loc = int(z['durations'][0][1] / 60)
+		distance_to_loc = z['distances'][0][1]
+
+		return time_to_loc, distance_to_loc
+	except:
+		if geocode_column == []:
+			time_to_loc = 0
+			distance_to_loc = 0
+			return time_to_loc, distance_to_loc
 
 #Export to excel
 def new_excel(DistMat_df):
@@ -136,17 +155,20 @@ def GeoGui():
 		#Loop
 	while True:
 		event, values = window.read()
+
 		if event == 'Generate':
 			file_read = values['ZiplabsReport']
 			try:
 				encoded_df = create_df(file_read)
-				print('Preview\n', encoded_df.head())
+				print('Preview\n', encoded_df.to_string())
 				window.Refresh()
 			except Exception as e: print(e)
+
 		if event == 'Generate Distance Matrix':
 			try:
 				DistMat_df = converter(encoded_df)
 			except Exception as e: print(e)
+
 		if event == 'Export':
 			try:
 				new_excel(DistMat_df)
@@ -154,9 +176,11 @@ def GeoGui():
 				print('Exporting New Excel...')
 				print('Successful!')
 				window.Refresh()
-			except Exception as e: print(e)			
+			except Exception as e: print(e)
+
 		if event == 'About':      
-			sg.popup('About this program', 'Version 1.0', 'Created by Emmanuel Kaunda') 
+			sg.popup('About this program', 'Version 1.0', 'Created by Emmanuel Kaunda')
+
 		if event == sg.WINDOW_CLOSED or event == 'Exit':
 			c.close()
 			break
